29d0ab6df9adec2523f03caefacad39f07d4a807
Jehangir Kazi <kazi@jehangir.me>
2017-02-27 08:33:54 +0500

    added timing

--- udpClient.js
+++ udpClient.js
@@ -2,18 +2,26 @@ var PORT = 5001;
 var HOST = '10.0.0.2';
 
 var dgram = require('dgram');
-var message = new Buffer('My KungFu is Good! i:   ');
+var message = new Buffer('packet#');
 var client = dgram.createSocket('udp4');
 
 client.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
+client.on('message', function (message, remote) {
+    // console.log(remote.address + ':' + remote.port +' - ' + message);
+    // server.send(message, 0, message.length, PORT, remote.address, function(err, bytes) {
+    //     if (err) throw err;
+    //     console.log('UDP message sent to ' + remote.address +':'+ PORT + message);
+    // });
+    console.timeEnd(Buffer.toString(message))
+});
 for (var i = 0; i < 100; i++) {
-    message = Buffer.concat([message, new Buffer(i)])
+    message = Buffer.concat([message, new Buffer(str(i))])
     client.send(message , 0, message.length, PORT, HOST, function(err, bytes) {
         if (err) throw err;
         console.log('UDP message sent to ' + HOST +':'+ PORT + ' i: ' + i);
-        console.time("packetSent" + str(i))
+        console.time(Buffer.toString(message))
     });
 }
--- udpServer.js
+++ udpServer.js
@@ -12,9 +12,7 @@ server.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
     server.send(message, 0, message.length, PORT, remote.address, function(err, bytes) {
         if (err) throw err;
-        console.log('UDP message sent to ' + remote.address +':'+ PORT + 'ACK');
-        console.time("packetSent")
-        
+        console.log('UDP message sent to ' + remote.address +':'+ PORT + message);
     });
 });
 
