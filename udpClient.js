2ce6e8ce9afbe3a13073dd7a9a0ba29d4650bdee
Jehangir Kazi <kazi@jehangir.me>
2017-02-27 08:40:44 +0500

    fixed timing

--- udpClient.js
+++ udpClient.js
@@ -2,7 +2,6 @@ var PORT = 5001;
 var HOST = '10.0.0.2';
 
 var dgram = require('dgram');
-var message = new Buffer('packet#');
 var client = dgram.createSocket('udp4');
 
 client.on('listening', function () {
@@ -18,6 +17,7 @@ client.on('message', function (message, remote) {
     console.timeEnd(Buffer.toString(message))
 });
 for (var i = 0; i < 100; i++) {
+    var message = new Buffer('packet#');
     message = Buffer.concat([message, new Buffer(i.toString())])
     client.send(message , 0, message.length, PORT, HOST, function(err, bytes) {
         if (err) throw err;
