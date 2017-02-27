a504ec88b1950321170f02ca81c9af1e59e0f8b7
Jehangir Kazi <kazi@jehangir.me>
2017-02-27 23:12:43 +0500

    buffer methods updated

--- udpClient.js
+++ udpClient.js
@@ -11,15 +11,14 @@ client.on('listening', function () {
 });
 client.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
-    console.log(Buffer.toString(message))
+    console.log(message.toString())
     // console.timeEnd(Buffer.toString(message))
 });
 for (var i = 0; i < 100; i++) {
-    var message = new Buffer('packet#');
-    message = Buffer.concat([message, new Buffer(i.toString())])
+    message = Buffer.concat([Buffer.from('packet#'), Buffer.from(i.toString())])
     client.send(message , 0, message.length, PORT, HOST, function(err, bytes) {
         if (err) throw err;
         // console.log('UDP message sent to ' + HOST +':'+ PORT + ' i: ' + i);
-        console.time(Buffer.toString(message))
+        console.time(message.toString())
     });
 }
