98922a79f5a3b5580be99958fa90ff6c61ea4ff4
Jehangir Kazi <kazi@jehangir.me>
2017-02-27 23:42:04 +0500

    put i in closure

--- udpClient.js
+++ udpClient.js
@@ -14,15 +14,12 @@ client.on('message', function (message, remote) {
     // console.log(message.toString())
     console.timeEnd(message.toString())
 });
-messages = []
-for (var i = 0; i < 100; i++) {
-    messages.push(Buffer.concat([Buffer.from('packet#'), Buffer.from(i.toString())]))
-}
-for (var i = 0; i < 100; i++) {
-    // message = Buffer.concat([Buffer.from('packet#'), Buffer.from(i.toString())])
-    client.send(messages[i] , 0, messages[i].length, PORT, HOST, function(err, bytes) {
-        if (err) throw err;
-        // console.log('UDP message sent to ' + HOST +':'+ PORT + ' i: ' + i);
-        console.time(messages[i].toString())
-    });
+for (var i = 0; i < 100; i++) { (i) => {
+        message = Buffer.concat([Buffer.from('packet#'), Buffer.from(i.toString())])
+        client.send(message , 0, message.length, PORT, HOST, function(err, bytes) {
+            if (err) throw err;
+            // console.log('UDP message sent to ' + HOST +':'+ PORT + ' i: ' + i);
+            console.time(message.toString())
+        });
+    } 
 }
